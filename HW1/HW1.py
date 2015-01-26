d135ee230d5814efd8695a38f100d755c1c6aa77
Kari Davis <kari.davis@rackspace.com>
2015-01-26 08:59:10 -0600

    first commit

---
+++ HW1/HW1.py
@@ -0,0 +1,36 @@
+__author__ = 'kari.davis'
+
+#simple Hello World in Python
+print("Hello World!")
+
+#Assignment Statement
+a = 'Hello World!'
+
+#Output in Console
+print(a)
+
+#Example of printing, print first byte, but it starts with 0
+print(a[0])
+print(a[1])
+print(a[2])
+print(a[3])
+print(a[4])
+print(a[5])
+print(a[6])
+print(a[7])
+print(a[8])
+print(a[9])
+print(a[10])
+print(a[11])
+
+#Example of printing in the middle of a string, beginning at location 3
+print(a[2:5])
+
+#Example of printing from byte #3 to end
+print(a[2:])
+
+#Eample of printing from beginning to middle, adding a character
+print(a[:3]+'p')
+
+#Example of printing and concatenation
+print(a + ' - ' + 'Test')
